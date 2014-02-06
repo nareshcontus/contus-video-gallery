3bdf159e526e7cca9b2296d8309a52bada2591d1
karthikeyanicontus <karthikeyani@contus.in>
2014-02-06 17:58:21 +0530

    Commit for Modified version in WordPress Video Gallery 2.6

--- hdflv-config.php
+++ hdflv-config.php
@@ -16,8 +16,8 @@
  * Define the server path to the file wp-config here, if you placed WP-CONTENT outside the classic file structure */
 $path = '';	   ## It should be end with a trailing slash
 ## That's all, stop editing from here
-if ( !defined( 'WP_LOAD_PATH' ) ) {
-## classic root path if wp-content and plugins is below wp-config.php
+if ( ! defined( 'WP_LOAD_PATH' ) ) {
+	## classic root path if wp-content and plugins is below wp-config.php
 	$classic_root = dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/';
 
 	if ( file_exists( $classic_root . 'wp-load.php' ) )
