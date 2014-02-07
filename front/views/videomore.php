85558f4d00b01df57db45eb3705bc5c5df963d54
karthikeyanicontus <karthikeyani@contus.in>
2014-02-07 11:57:17 +0530

    Commit for Modified version in WordPress Video Gallery 2.6

--- front/views/videomore.php
+++ front/views/videomore.php
@@ -34,17 +34,17 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 			$this->_viewslang = __( 'Views', 'video_gallery' );
 			$this->_viewlang  = __( 'View', 'video_gallery' );
 			## Get search keyword
-			$searchVal     = str_replace( ' ', '%20', __( 'Video Search ...', 'video_gallery' ) );
+			$searchVal = str_replace( ' ', '%20', __( 'Video Search ...', 'video_gallery' ) );
 			if ( isset( $wp_query->query_vars['video_search'] ) && $wp_query->query_vars['video_search'] !== $searchVal ) {
 				$video_search = $wp_query->query_vars['video_search'];
 			}
 			$this->_video_search = $video_search;
 
-			$this->_showF  = 5;
-			$this->_colF   = $this->_settingsData->colMore;							## get row of more page
-			$this->_colCat = $this->_settingsData->colCat;							## get column of more page
-			$this->_rowCat = $this->_settingsData->rowCat;							## get row of category videos
-			$this->_perCat = $this->_colCat * $this->_rowCat;						## get column of category videos
+			$this->_showF     = 5;
+			$this->_colF      = $this->_settingsData->colMore;							## get row of more page
+			$this->_colCat    = $this->_settingsData->colCat;							## get column of more page
+			$this->_rowCat    = $this->_settingsData->rowCat;							## get row of category videos
+			$this->_perCat    = $this->_colCat * $this->_rowCat;						## get column of category videos
 			$dir = dirname( plugin_basename( __FILE__ ) );
 			$dirExp = explode( '/', $dir );
 			$this->_folder    = $dirExp[0];											## Get plugin folder name
@@ -87,9 +87,9 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 						$where = 'AND w.featured=1';
 						$rowF  = $this->_settingsData->rowMore;
 						$colF  = $this->_settingsData->colMore;
-						$dataLimit     = $rowF * $colF;
-						$TypeOFvideos  = $this->home_thumbdata( $thumImageorder, $where, $this->_pagenum, $dataLimit );
-						$CountOFVideos = $this->countof_videos( '', '', $thumImageorder, $where );
+						$dataLimit      = $rowF * $colF;
+						$TypeOFvideos   = $this->home_thumbdata( $thumImageorder, $where, $this->_pagenum, $dataLimit );
+						$CountOFVideos  = $this->countof_videos( '', '', $thumImageorder, $where );
 						$typename  = __( 'Featured', 'video_gallery' );
 						$type_name = 'featured';
 						$morePage  = '&more=fea';
@@ -100,21 +100,21 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 						$rowF  = $this->_settingsData->rowCat;
 						$colF  = $this->_settingsData->colCat;
 						$dataLimit = $rowF * $colF;
-						$TypeOFvideos  = $this->home_catthumbdata( $thumImageorder, $this->_pagenum, $dataLimit );
-						$CountOFVideos = $this->countof_videos( $this->_playid, '', $thumImageorder, $where );
-						$typename      = __( 'Category', 'video_gallery' );
-						$morePage      = '&playid=' . $thumImageorder;
+						$TypeOFvideos   = $this->home_catthumbdata( $thumImageorder, $this->_pagenum, $dataLimit );
+						$CountOFVideos  = $this->countof_videos( $this->_playid, '', $thumImageorder, $where );
+						$typename       = __( 'Category', 'video_gallery' );
+						$morePage       = '&playid=' . $thumImageorder;
 						break;
 					case 'user':
 						$thumImageorder = $this->_userid;
 						$where = '';
 						$rowF  = $this->_settingsData->rowCat;
 						$colF  = $this->_settingsData->colCat;
-						$dataLimit     = $rowF * $colF;
-						$TypeOFvideos  = $this->home_userthumbdata( $thumImageorder, $this->_pagenum, $dataLimit );
-						$CountOFVideos = $this->countof_videos( '', $this->_userid, $thumImageorder, $where );
-						$typename      = __( 'User', 'video_gallery' );
-						$morePage      = '&userid=' . $thumImageorder;
+						$dataLimit      = $rowF * $colF;
+						$TypeOFvideos   = $this->home_userthumbdata( $thumImageorder, $this->_pagenum, $dataLimit );
+						$CountOFVideos  = $this->countof_videos( '', $this->_userid, $thumImageorder, $where );
+						$typename       = __( 'User', 'video_gallery' );
+						$morePage       = '&userid=' . $thumImageorder;
 						break;
 					case 'search':
 						$video_search = str_replace( '%20', ' ', $this->_video_search );
@@ -130,9 +130,9 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 						$rowF = $this->_settingsData->rowMore;
 						$colF = $this->_settingsData->colMore;
 						$dataLimit = $rowF * $colF;
-						$TypeOFvideos  = $this->home_searchthumbdata( $thumImageorder, $this->_pagenum, $dataLimit );
-						$CountOFVideos = $this->countof_videosearch( $thumImageorder );
-						return $this->searchList( $video_search, $CountOFVideos, $TypeOFvideos, $this->_pagenum, $dataLimit );
+						$TypeOFvideos   = $this->home_searchthumbdata( $thumImageorder, $this->_pagenum, $dataLimit );
+						$CountOFVideos  = $this->countof_videosearch( $thumImageorder );
+						return $this->searchlist( $video_search, $CountOFVideos, $TypeOFvideos, $this->_pagenum, $dataLimit );
 						break;
 					case 'categories':
 					default:
@@ -142,37 +142,37 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 						$TypeOFvideos  = $this->home_categoriesthumbdata( $this->_pagenum, $dataLimit );
 						$CountOFVideos = $this->countof_videocategories();
 						$typename = __( 'Video Categories', 'video_gallery' );
-						return $this->categoryList( $CountOFVideos, $TypeOFvideos, $this->_pagenum, $dataLimit );
+						return $this->categorylist( $CountOFVideos, $TypeOFvideos, $this->_pagenum, $dataLimit );
 						break;
 				}
 
-				$class = $div = '';
+				$div = '';
 				$ratearray = array( 'nopos1', 'onepos1', 'twopos1', 'threepos1', 'fourpos1', 'fivepos1' );
 				?>
 
 				<?php
 
 				$pagenum = isset( $this->_pagenum ) ? absint( $this->_pagenum ) : 1;
-				$div = '<div class="video_wrapper" id="' . $type_name . '_video">';
-				$div .= '<style type="text/css"> .video-block {  margin-left:' . $this->_settingsData->gutterspace . 'px !important; } </style>';
+				$div     = '<div class="video_wrapper" id="' . $type_name . '_video">';
+				$div    .= '<style type="text/css"> .video-block {  margin-left:' . $this->_settingsData->gutterspace . 'px !important; } </style>';
 				if ( $typename == 'Category' ) {
 					$playlist_name = get_playlist_name( intval( $this->_playid ) );
-					$div .='<h2 >' . $playlist_name . ' </h2>';
+					$div .= '<h2 >' . $playlist_name . ' </h2>';
 				} else if ( $typename == 'User' ) {
 					$user_name = get_user_name( intval( $this->_userid ) );
-					$div .='<h2 >' . $user_name . ' </h2>';
+					$div .= '<h2 >' . $user_name . ' </h2>';
 				} else {
-					$div .='<h2 >' . $typename . ' ' . __( 'Videos', 'video_gallery' ) . ' </h2>';
+					$div .= '<h2 >' . $typename . ' ' . __( 'Videos', 'video_gallery' ) . ' </h2>';
 				}
 				if ( ! empty( $TypeOFvideos ) ) {
 					$j = 0;
 					$fetched[$j] = '';
-					$image_path = str_replace( 'plugins/' . $this->_folder . '/', 'uploads/videogallery/', APPTHA_VGALLERY_BASEURL );
+					$image_path  = str_replace( 'plugins/' . $this->_folder . '/', 'uploads/videogallery/', APPTHA_VGALLERY_BASEURL );
 					foreach ( $TypeOFvideos as $video ) {
 						$duration[$j] = $video->duration;									## VIDEO DURATION
 						$imageFea[$j] = $video->image;										## VIDEO IMAGE
-						$file_type = $video->file_type;										## Video Type
-						$guid[$j] = get_video_permalink( $video->slug );						## guid
+						$file_type    = $video->file_type;										## Video Type
+						$guid[$j]     = get_video_permalink( $video->slug );						## guid
 						if ( $imageFea[$j] == '' ) {											## If there is no thumb image for video
 							$imageFea[$j] = $this->_imagePath . 'nothumbimage.jpg';
 						} else {
@@ -180,21 +180,21 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 								$imageFea[$j] = $image_path . $imageFea[$j];
 							}
 						}
-						$vidF[$j] = $video->vid;											## VIDEO ID
-						$nameF[$j] = $video->name;											## VIDEI NAME
-						$hitcount[$j] = $video->hitcount;									## VIDEO HITCOUNT
+						$vidF[$j]      = $video->vid;											## VIDEO ID
+						$nameF[$j]     = $video->name;											## VIDEI NAME
+						$hitcount[$j]  = $video->hitcount;									## VIDEO HITCOUNT
 						$ratecount[$j] = $video->ratecount;									## VIDEO RATECOUNT
 						$rate[$j] = $video->rate;											## VIDEO RATE
 						if ( ! empty( $this->_playid ) ) {
-							$fetched[$j] = $video->playlist_name;
+							$fetched[$j]       = $video->playlist_name;
 							$fetched_pslug[$j] = $video->playlist_slugname;
-							$playlist_id[$j] = $this->_playid;
+							$playlist_id[$j]   = $this->_playid;
 						} else {
 							$getPlaylist = $this->_wpdb->get_row( 'SELECT playlist_id FROM ' . $this->_wpdb->prefix . 'hdflvvideoshare_med2play WHERE media_id="' . intval( $vidF[$j] ) . '"' );
 							if ( isset( $getPlaylist->playlist_id ) ) {
-								$playlist_id[$j] = $getPlaylist->playlist_id;				## VIDEO CATEGORY ID
-								$fetPlay[$j] = $this->_wpdb->get_row( 'SELECT playlist_name,playlist_slugname FROM ' . $this->_wpdb->prefix . 'hdflvvideoshare_playlist WHERE pid="' . intval( $playlist_id[$j] ) . '"' );
-								$fetched[$j] = $fetPlay[$j]->playlist_name;					## CATEOGORY NAME
+								$playlist_id[$j]   = $getPlaylist->playlist_id;				## VIDEO CATEGORY ID
+								$fetPlay[$j]       = $this->_wpdb->get_row( 'SELECT playlist_name,playlist_slugname FROM ' . $this->_wpdb->prefix . 'hdflvvideoshare_playlist WHERE pid="' . intval( $playlist_id[$j] ) . '"' );
+								$fetched[$j]       = $fetPlay[$j]->playlist_name;					## CATEOGORY NAME
 								$fetched_pslug[$j] = $fetPlay[$j]->playlist_slugname;		## CATEOGORY NAME
 							}
 						}
@@ -263,31 +263,31 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 																							## PAGINATION STARTS
 				$total = $CountOFVideos;
 				$num_of_pages = ceil( $total / $dataLimit );
-				$page_links = paginate_links( 
-						array( 
+				$page_links   = paginate_links(
+						array(
 							'base' => add_query_arg( 'pagenum', '%#%' ),
 							'format' => '',
 							'prev_text' => __( '&laquo;', 'aag' ),
 							'next_text' => __( '&raquo;', 'aag' ),
 							'total' => $num_of_pages,
 							'current' => $pagenum,
-							 )
-						 );
+							)
+						);
 
 				if ( $page_links ) {
-					$div .='<div class="tablenav"><div class="tablenav-pages" >' . $page_links . '</div></div>';
+					$div .= '<div class="tablenav"><div class="tablenav-pages" >' . $page_links . '</div></div>';
 				}
 																							## PAGINATION ENDS
 				return $div;
 			}
 		}
 
-		function categoryList( $CountOFVideos, $TypeOFvideos, $pagenum, $dataLimit ) {
+		function categorylist( $CountOFVideos, $TypeOFvideos, $pagenum, $dataLimit ) {
 
 			global $wpdb;
 			$div = '';
-			$pagenum = isset( $pagenum ) ? absint( $pagenum ) : 1;								## Calculating page number
-			$start   = (  $pagenum - 1  ) * $dataLimit;										## Video starting from
+			$pagenum   = isset( $pagenum ) ? absint( $pagenum ) : 1;								## Calculating page number
+			$start     = (  $pagenum - 1  ) * $dataLimit;										## Video starting from
 			$ratearray = array( 'nopos1', 'onepos1', 'twopos1', 'threepos1', 'fourpos1', 'fivepos1' );
 			?>
 
@@ -300,10 +300,10 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 						INNER JOIN ' . $wpdb->prefix . 'hdflvvideoshare_playlist AS p on m.playlist_id = p.pid
 						INNER JOIN ' . $this->_wpdb->prefix . 'posts s ON s.ID=w.slug
 						WHERE w.publish=1 AND p.is_publish=1 AND m.playlist_id=' . intval( $catList->pid ) . ' GROUP BY w.vid';
-				$playLists = $wpdb->get_results( $sql );
+				$playLists     = $wpdb->get_results( $sql );
 				$playlistCount = count( $playLists );
 
-				$div .='<div> <h4 class="clear more_title">' . $catList->playlist_name . '</h4></div>';
+				$div .= '<div> <h4 class="clear more_title">' . $catList->playlist_name . '</h4></div>';
 				if ( ! empty( $playlistCount ) ) {
 					$i = 0;
 					$inc = 1;
@@ -311,8 +311,8 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 					$div .= '<ul class="video-block-container">';
 					foreach ( $playLists as $playList ) {
 
-						$duration = $playList->duration;
-						$imageFea = $playList->image;										## VIDEO IMAGE
+						$duration  = $playList->duration;
+						$imageFea  = $playList->image;										## VIDEO IMAGE
 						$file_type = $playList->file_type;									## Video Type
 						$guid = get_video_permalink( $playList->slug );						## guid
 						if ( $imageFea == '' ) {												## If there is no thumb image for video
@@ -379,16 +379,16 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 			## PAGINATION STARTS
 			$total = $CountOFVideos;
 			$num_of_pages = ceil( $total / $dataLimit );
-			$page_links   = paginate_links( 
-					array( 
+			$page_links   = paginate_links(
+					array(
 						'base' => add_query_arg( 'pagenum', '%#%' ),
 						'format' => '',
 						'prev_text' => __( '&laquo;', 'aag' ),
 						'next_text' => __( '&raquo;', 'aag' ),
 						'total' => $num_of_pages,
 						'current' => $pagenum,
-						 )
-					 );
+						)
+					);
 
 			if ( $page_links ) {
 				$div .= '<div class="tablenav"><div class="tablenav-pages" >' . $page_links . '</div></div>';
@@ -398,25 +398,25 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 			return $div;
 		}
 
-		function searchList( $video_search, $CountOFVideos, $TypeOFvideos, $pagenum, $dataLimit ) {
+		function searchlist( $video_search, $CountOFVideos, $TypeOFvideos, $pagenum, $dataLimit ) {
 			$div = '';
-			$pagenum = isset( $pagenum ) ? absint( $pagenum ) : 1;					## Calculating page number
-			$start   = (  $pagenum - 1  ) * $dataLimit;								## Video starting from
-			$limit   = $dataLimit;													## Video Limit
+			$pagenum   = isset( $pagenum ) ? absint( $pagenum ) : 1;					## Calculating page number
+			$start     = (  $pagenum - 1  ) * $dataLimit;								## Video starting from
+			$limit     = $dataLimit;													## Video Limit
 			$ratearray = array( 'nopos1', 'onepos1', 'twopos1', 'threepos1', 'fourpos1', 'fivepos1' );
-			$div .='<div class="video_wrapper" id="video_search_result"><h3 class="entry-title">' . __( 'Search Results', 'video_gallery' ) . ' - ' . $video_search . '</h3>';
+			$div .= '<div class="video_wrapper" id="video_search_result"><h3 class="entry-title">' . __( 'Search Results', 'video_gallery' ) . ' - ' . $video_search . '</h3>';
 			$div .= '<style> .video-block { margin-left:' . $this->_settingsData->gutterspace . 'px !important; } </style>';
 
 			## Fetch videos for every category
 			if ( ! empty( $TypeOFvideos ) ) {
-				$i =  $inc = 0;
+				$i  = $inc  = 0;
 				$image_path = str_replace( 'plugins/' . $this->_folder . '/', 'uploads/videogallery/', APPTHA_VGALLERY_BASEURL );
 				$div .= '<ul class="video-block-container">';
 
 				foreach ( $TypeOFvideos as $playList ) {
 
-					$duration = $playList->duration;
-					$imageFea = $playList->image;									## VIDEO IMAGE
+					$duration  = $playList->duration;
+					$imageFea  = $playList->image;									## VIDEO IMAGE
 					$file_type = $playList->file_type;								## Video Type
 					$guid = get_video_permalink( $playList->slug );					## guid
 					if ( $imageFea == '' ) {											## If there is no thumb image for video
@@ -453,7 +453,7 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 						$div .= '<span class="ratethis1 ' . $ratearray[$ratestar] . '"></span>';
 					}
 						## Rating ends and views starts here
-						if ( $this->_settingsData->view_visible == 1 ) {
+					if ( $this->_settingsData->view_visible == 1 ) {
 						if ( $playList->hitcount > 1 ) {
 							$viewlang = $this->_viewslang;
 						} else {
@@ -478,16 +478,16 @@ if ( class_exists( 'ContusMoreView' ) != true ) {
 			$num_of_pages = ceil( $total / $dataLimit );
 			$video_search = str_replace( ' ', '%20', $video_search );
 			$arr_params   = array( 'pagenum' => '%#%' );
-			$page_links   = paginate_links( 
-					array( 
+			$page_links = paginate_links(
+					array(
 						'base' => add_query_arg( $arr_params ),
 						'format' => '',
 						'prev_text' => __( '&laquo;', 'aag' ),
 						'next_text' => __( '&raquo;', 'aag' ),
 						'total' => $num_of_pages,
 						'current' => $pagenum,
-						 )
-					 );
+						)
+					);
 
 			if ( $page_links ) {
 				$div .= '<div class="tablenav"><div class="tablenav-pages" >' . $page_links . '</div></div>';
